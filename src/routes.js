02a9fb651880595aa4181dc9aebe805982ebf94a
darshangit <darshans1989@gmail.com>
2017-10-29 20:10:07 +0530

    Working model of the App created. basic working layout

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
-import { React } from 'react';
-import { Router, IndexRoute } from "react-router";
-import { App } from "./components/App";
-import { HomePage } from "./components/home/HomePage";
-import { AboutPage } from "./components/about/AboutPage";
+import React from 'react';
+import { Route, IndexRoute } from "react-router";
+import App from "./components/App";
+import HomePage from "./components/home/HomePage";
+import AboutPage from "./components/about/AboutPage";
 
 export default (
     <Route path="/" component={App}>
